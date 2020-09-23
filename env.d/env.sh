9b332191419ca31b5bf6f244dd7b3bdec165b716
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2020-09-23 19:43:33 +0200

    basic env

---
+++ env.d/env.sh
@@ -0,0 +1,5 @@
+ODA_WORKER_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
+
+export TERM=xterm
+
+source $ODA_WORKER_ROOT/env.d/local-pyenv.sh
---
+++ env.d/local-pyenv.sh
@@ -0,0 +1,5 @@
+export PYENV_ROOT="$ODA_WORKER_ROOT/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+eval "$(pyenv init -)"
+
