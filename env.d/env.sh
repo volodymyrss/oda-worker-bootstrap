b360290c5eb57dd45b594f85df5465606145df8f
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2020-09-26 10:23:25 +0200

    requirments

--- env.d/env.sh
+++ env.d/env.sh
@@ -3,3 +3,33 @@ ODA_WORKER_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && 
 export TERM=xterm
 
 source $ODA_WORKER_ROOT/env.d/local-pyenv.sh
+
+ls -lotr ~/.dda-token 
+chmod 600 ~/.dda-token 
+
+export ODAHUB=https://dqueue.staging-1-3.odahub.io@queue-osa11
+export DDA_QUEUE=$ODAHUB
+
+
+export CURRENT_IC=/isdc/arc/rev_3/
+#export ISDC_ENV=/unsaved/astro/savchenk/osa11_deployment/deployment/CentOS_7.7.1908_x86_64/osa11
+export ISDC_ENV=/unsaved/astro/savchenk/software/osa/10.2/
+source $ISDC_ENV/bin/isdc_init_env.sh
+
+export HEADAS=/unsaved/astro/savchenk/osa11_deployment/deployment/CentOS_7.7.1908_x86_64/heasoft-versions/6.25/ae75cac5-20191219-121436/x86_64-pc-linux-gnu-libc2.17/
+source $HEADAS/headas-init.sh 
+
+export ISDC_REF_CAT=/isdc/arc/rev_3/cat/hec/gnrl_refr_cat_0043.fits 
+export REP_BASE_PROD=/isdc/arc/rev_3/cat/hec/gnrl_refr_cat_0043.fits 
+export REP_BASE_PROD=/isdc/arc/rev_3/
+export INTEGRAL_DDCACHE_ROOT=$PWD/data/reduced/ddcache
+export INTEGRAL_DATA=$REP_BASE_PROD/
+export ISGRI_RESPONSE=/unsaved/astro/savchenk/data/resources/rmf_62bands.fits 
+
+pip install pip --upgrade
+pip install -r requirements.txt
+
+dqueue info
+
+
+python -m dataanalysis.caches.queue $ODAHUB
--- env.d/local-pyenv.sh
+++
@@ -1,5 +0,0 @@
-export PYENV_ROOT="$ODA_WORKER_ROOT/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-
-eval "$(pyenv init -)"
-
