a1a2adbdc4cf7c88271f927d0796c1876ed4a28f
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2020-11-14 15:45:36 +0100

    track worker id, accept failure

--- runner-slurm.sh
+++ runner-slurm.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
+export PYTHONUNBUFFERED=1
 ODA_WORKER_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
-
-cd /scratch/savchenk/oda-runner
-source env.d/env.sh
 
 pip install --user data-analysis oda-node --upgrade
 
@@ -11,12 +9,36 @@ dqueue version
 
 d=/scratch/savchenk/odawtmp$$-$RANDOM
 
+export EXIT_WITHOUT_INTEGRAL_ARCHIVE=yes
+
 mkdir -pv $d
 
-(
-    cd $d
+for n in $(seq 1 5); do
 
-cat -> worker-knowledge.yaml <<HERE
+    (
+        export OSA=11.0
+        cd $d
+        echo -e "\033[32mworker for $OSA in $PWD\033[0m"
+
+    cat -> worker-knowledge.yaml <<HERE
+- any-of:
+  - key: ['object_identity', 'modules']
+    value: ['git', 'ddosa11/staging-1-3', 'None']
+  - key: ['object_identity', 'modules']
+    value: ['git', 'ddosa11', 'git://ddosa11/staging-1-3']
+HERE
+        source ~/init-osa.sh
+
+        python -m dataanalysis.caches.queue $ODAHUB -B 3 -k  worker-knowledge.yaml -n ${HOSTNAME}-${OSA}-${SLURM_JOBID:-notajob}
+
+    )
+    
+    (
+        cd $d
+        export OSA=10.2
+        echo -e "\033[32mworker for $OSA in $PWD\033[0m"
+
+    cat -> worker-knowledge.yaml <<HERE
 - none-of:
   - key: ['object_identity', 'modules']
     value: ['git', 'ddosa11/staging-1-3', 'None']
@@ -29,27 +51,9 @@ cat -> worker-knowledge.yaml <<HERE
     value: ['git', 'ddosa', 'git://ddosa/staging-1-3']
 HERE
 
-    export OSA=10.2
-    ~/init-osa.sh
+        source ~/init-osa.sh
 
-    python -m dataanalysis.caches.queue $ODAHUB -B 3 -k  worker-knowledge.yaml
+        python -m dataanalysis.caches.queue $ODAHUB -B 1 -k  worker-knowledge.yaml -n ${HOSTNAME}-${OSA}-${SLURM_JOBID:-notajob}
 
-)
-
-(
-    cd $d
-
-cat -> worker-knowledge.yaml <<HERE
-- any-of:
-  - key: ['object_identity', 'modules']
-    value: ['git', 'ddosa11/staging-1-3', 'None']
-  - key: ['object_identity', 'modules']
-    value: ['git', 'ddosa11', 'git://ddosa11/staging-1-3']
-HERE
-    export OSA=11.0
-    ~/init-osa.sh
-
-    python -m dataanalysis.caches.queue $ODAHUB -B 3 -k  worker-knowledge.yaml
-
-)
-
+    )
+done
