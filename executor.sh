c462840f4055d5fa59bb6f8850381a7755ad238c
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2020-11-16 10:36:09 +0100

    new executor and runner files

---
+++ executor.sh
@@ -0,0 +1,5 @@
+while true; do 
+    python -m dqueue.cli runner start-executor -m 30 -t 10 -n 0 \
+        'd=logs/{dt.year}-{dt.month:02d}/{dt.day:02d}/{dt.hour:02d}-{dt.minute:02d}/; mkdir -pv $d; sbatch --exclude=node075 -o $d/log-{hostname}-{pid}-{age:06d}.txt runner-slurm.sh' \
+        'squeue -u savchenk | grep -v JOBID || true'; 
+done > log 2>&1
---
+++ runner.sh
@@ -0,0 +1,15 @@
+ODA_WORKER_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
+
+source env.d/env.sh
+
+export PYTHONPATH=$PWD/data-analysis:$PYTHONPATH
+export PYTHONPATH=$PWD/dqueue:$PYTHONPATH
+
+dqueue version
+
+mkdir runner-$OSA
+cd runner-$OSA
+
+#pyinstrument -o myLog.profile -m dataanalysis.caches.queue $ODAHUB -B 1 -k  ../worker-knowledge-osa${OSA}.yaml
+pyinstrument -o myLog.profile -m dataanalysis.caches.queue $ODAHUB -B 1000 -k  ../worker-knowledge-osa${OSA}.yaml
+
