7ec5b21e6a7c47d5f6679a61d1b427ee6618f55a
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2021-02-10 09:59:42 +0100

    new resources

--- executor.sh
+++ executor.sh
@@ -1,4 +1,10 @@
-echo "scratch_root: ${scratch_root:=/hpcstorage/savchenk/oda-runner/}"
+#!/bin/bash
+
+set -xe -o pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "scratch_root: ${scratch_root:=/scratch/savchenk/oda-runner/}"
 echo "log_root: ${log_root:=/hpcstorage/savchenk/oda-runner/}"
 
 export scratch_root
@@ -6,8 +12,12 @@ export log_root
 
 pip install --upgrade oda-node
 
+export LOGSTASH_ENTRYPOINT=cdcihn.isdc.unige.ch:5001
+
 while true; do 
     python -m dqueue.cli runner start-executor -m 30 -t 10 -n -5 \
-        -d 'd='${log_root}/'logs/{dt.year}-{dt.month:02d}/{dt.day:02d}/{dt.hour:02d}-{dt.minute:02d}/; mkdir -pv $d; sbatch --export=scratch_root --exclude=node075 -o $d/log-{hostname}-{pid}-{age:06d}.txt runner-slurm.sh' \
-        -l 'squeue -u savchenk | grep -v JOBID  || true'; 
+           -d 'd='${log_root}/'logs/{dt.year}-{dt.month:02d}/{dt.day:02d}/{dt.hour:02d}-{dt.minute:02d}/; 
+               mkdir -pv $d
+               sbatch --export=scratch_root --exclude=node075 -o $d/log-{hostname}-{pid}-{age:06d}.txt '$DIR'/runner-slurm.sh' \
+           -l 'squeue -u savchenk | grep -v JOBID  || true'; 
 done 
